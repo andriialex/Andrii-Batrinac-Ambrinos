3924178e33c43c8943d566c540d6fac5de6cd23a
andriialex <103776334+andriialex@users.noreply.github.com>
2022-12-21 22:05:59 +0200

    more changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Register from "./pages/Register";
+import Homepage from "./pages/Homepage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Homepage/>,
+  },
+  {
+    path: "/register",
+    element:<Register/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
